7a5ab201a58d30748f01dbf953721aec16f8aa5f
sdlfkorg <sdlfkorg@gmail.com>
2018-05-06 20:42:06 +0800

    compile.js: fix the name of the file to be complied

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -10,11 +10,12 @@ const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath); //check the dir is existed. If not, create the dir
-// console.log('source');
-// console.log(source);
+// console.log('output');
+// console.log(output);
+
 for (let contract in output){
 	fs.outputJsonSync(
-		path.resolve(buildPath, contract + '.json'),
+		path.resolve(buildPath, contract.replace(':', '') + '.json'),
 		output[contract]
 	);
 }
