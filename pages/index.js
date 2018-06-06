789173ceff5427b1674124a424b829cf3f0dcec4
sdlfkorg <sdlfkorg@gmail.com>
2018-06-06 14:42:49 +0800

    add next pages

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import factory from '../ethereum/factory';
+
+
+class CampaignIndex extends Component {
+	async componentDidMount() {
+		const campaigns = await factory.methods.getDeployedCampaigns().call();
+		console.log(campaigns);
+	}
+
+	render(){
+		return (
+			<div>Campaign index</div>
+		);
+	}
+}
+
+export default CampaignIndex;
