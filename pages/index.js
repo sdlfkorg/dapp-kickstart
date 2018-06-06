1d93aff4829d5963ac66faf4cd2020030ff63401
sdlfkorg <sdlfkorg@gmail.com>
2018-06-06 21:13:25 +0800

    build card list

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import {Card, Button} from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 
 class CampaignIndex extends Component {
-	async componentDidMount() {
+	static async getInitialProps(){
 		const campaigns = await factory.methods.getDeployedCampaigns().call();
-		console.log(campaigns);
+
+		return {campaigns}
+	}
+
+	renderCampaigns(){
+		const items = this.props.campaigns.map(asddress => {
+			return {
+				header: asddress,
+				description: <a>View Campaign</a>,
+				fluid: true
+			}
+		});
+
+		return <Card.Group items={items} />;
 	}
 
 	render(){
 		return (
-			<div>Campaign index</div>
+			<div>
+				<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
+				{this.renderCampaigns()}
+				<Button
+					content="Create Campaign"
+					icon="add circle"
+					primary
+				/>
+			</div>
 		);
 	}
 }
