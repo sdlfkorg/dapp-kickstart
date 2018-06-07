d386666a1c2e6582c3bb9aa3810a434cbb895040
sdlfkorg <sdlfkorg@gmail.com>
2018-06-07 16:14:29 +0800

    styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import { Container } from 'semantic-ui-react';
 
 export default (props) => {
 	return (
-		<div>
+		<Container>
 			<Header/>
 			{props.children}
 			<h1>Here will be a footer</h1>
-		</div>
+		</Container>
 	);	
 }
--- pages/index.js
+++ pages/index.js
@@ -30,12 +30,13 @@ class CampaignIndex extends Component {
 				<div>
 					<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"></link>
 					<h3>Open Campaign</h3>
-					{this.renderCampaigns()}
 					<Button
+						floated="right"
 						content="Create Campaign"
 						icon="add circle"
 						primary
 					/>
+					{this.renderCampaigns()}
 				</div>
 			</Layout>
 		);
