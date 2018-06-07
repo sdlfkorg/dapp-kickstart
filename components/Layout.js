3f71ce123866cdd6c6bc388ebd7c8e1cf5db24e2
sdlfkorg <sdlfkorg@gmail.com>
2018-06-07 13:27:30 +0800

    components-Layout:  use menu header

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
 export default (props) => {
 	return (
 		<div>
-			<h1>Here will be a header</h1>
+			<Header/>
 			{props.children}
 			<h1>Here will be a footer</h1>
 		</div>
