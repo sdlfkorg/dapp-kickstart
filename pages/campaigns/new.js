fe2ef5e10d7b6d910c9fc24e6d1b89851b3153c8
sdlfkorg <sdlfkorg@gmail.com>
2018-06-11 17:32:24 +0800

    pages-new: add simple form validation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Input, Message } from 'semantic-ui-react'
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
+
 
 class CampaignNew extends Component {
+	state = {
+		minumumContribution: '',
+		errorMessage: ''
+	}
+
+	onSubmit = async (event) => {
+		event.preventDefault();
+		try{
+			const accounts = await web3.eth.getAccounts();
+			await factory.methods
+				.createCampaign(this.state.minumumContribution)
+				.send({
+					from: accounts[0]
+				})
+		} catch(error) {
+			this.setState({errorMessage: error.message});
+		}
+		
+
+	}
+
 	render(){
 		return (
 			<Layout>
 				<h3>Create a New Campaign</h3>
 
-				<Form>
+				<Form onSubmit={this.onSubmit} error={!!this.state.errorMessage} >
 					<Form.Field>
 						<label>Minumum Contribution</label>
-						<input />
+						<Input 
+							label="wei" 
+							labelPosition="right"
+							value={this.state.minumumContribution}
+							onChange={event => 
+								{this.setState({minumumContribution: event.target.value})}}
+							 />
 					</Form.Field>
+					<Message error header="Oops!" content={this.state.errorMessage} />
 					<Button primary>Create!</Button>
 				</Form>
 			</Layout>
