bd63a0ab010b7e458489d5e22c23380efdd2a754
sdlfkorg <sdlfkorg@gmail.com>
2018-06-11 15:56:46 +0800

    page-new: add simple form layout

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import { Button, Form } from 'semantic-ui-react'
 
 class CampaignNew extends Component {
 	render(){
 		return (
 			<Layout>
-				<h1>New Campaign Page</h1>
+				<h3>Create a New Campaign</h3>
+
+				<Form>
+					<Form.Field>
+						<label>Minumum Contribution</label>
+						<input />
+					</Form.Field>
+					<Button primary>Create!</Button>
+				</Form>
 			</Layout>
 		);
 	}
