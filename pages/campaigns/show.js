8942c3db668bd676baeaa0efa321b6ad71a1a30d
sdlfkorg <sdlfkorg@gmail.com>
2018-06-19 17:21:07 +0800

    routes: setup dynamic routes

---
+++ pages/campaigns/show.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Layout from '../../components/Layout';
+
+class CampaignShow extends Component {
+	render(){
+		return (
+			<Layout>
+			<h3>CampaignShow page</h3>
+			</Layout>
+		);
+	}
+}
+
+export default CampaignShow;
