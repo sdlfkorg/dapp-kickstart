cfddc5434fb50573acf14082bf74c89e35881c79
sdlfkorg <sdlfkorg@gmail.com>
2018-06-20 14:49:02 +0800

    add contract function, compile and re-deploy the contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
 	JSON.parse(CampaignFactory.interface),
-	'0x1f7F6ab13A6ef779DBe060241af54C2757aBe23F'
+	'0x5Da8fA7F36e0db81d05120Bf23156889959D6788'
 );
 
 export default instance;
