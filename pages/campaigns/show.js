7b2af3ef5415c908d28d38551a867f3b0627b86c
sdlfkorg <sdlfkorg@gmail.com>
2018-06-21 14:30:33 +0800

    page-campaign-show: add card group and form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Card} from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
 	static async getInitialProps(props){
@@ -27,12 +29,43 @@ class CampaignShow extends Component {
 			approversCount,
 			manager
 		} = this.props;
-		const items = [{
-			header: manager,
-			meta: 'Address of manager',
-			description: 'The manager created this campaign and can create request to withdrawn money.',
-			style: {overflowWrap: 'break-word'}
-		}]
+
+		const items = [
+			{
+				header: manager,
+				meta: 'Address of manager',
+				description: 'The manager created this campaign and can create request to withdrawn money.',
+				style: {overflowWrap: 'break-word'}
+			},
+			{
+				header: minimumContribution,
+				meta: 'Minimum Contribution (wei)',
+				description: 'You must contribute at least this mush wei to become a approver.',
+				style: {overflowWrap: 'break-word'}
+			},
+			{
+				header: requestCount,
+				meta: 'Number of Requests',
+				description: 'A request tires to withdraw money from the contract. Request must be approved by the approvers.',
+				style: {overflowWrap: 'break-word'}
+			},
+			{
+				header: approversCount,
+				meta: 'Number of Approvers',
+				description: 'Number of people who have already donated to this campaign.',
+				style: {overflowWrap: 'break-word'}
+			},
+			{
+				header: web3.utils.fromWei(balance, 'ether'),
+				meta: 'Campaign Balance (ether)',
+				description: 'The balance is how much money has left to spend.',
+				style: {overflowWrap: 'break-word'}
+			}
+			
+		];
+
+
+
 
 		return <Card.Group items={items} />;
 	}
@@ -42,8 +75,10 @@ class CampaignShow extends Component {
 	render(){
 		return (
 			<Layout>
-			<h3>CampaignShow page</h3>
-			{this.renderCards()}
+				<h3>CampaignShow page</h3>
+				{this.renderCards()}
+				<ContributeForm>
+				</ContributeForm>
 			</Layout>
 		);
 	}
