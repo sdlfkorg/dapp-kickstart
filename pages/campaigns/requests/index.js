a0edc21bd200bdfa8d557eccf9eb715a3f28b668
sdlfkorg <sdlfkorg@gmail.com>
2018-06-22 17:31:43 +0800

    page-requests: setup create request button and link

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
+import Layout from '../../../components/Layout';
+import { Link } from '../../../routes';
+import {Button} from 'semantic-ui-react';
 
 class RequestIndex extends Component {
+	static async getInitialProps(props){
+		const {address} = props.query;
+		return {address: address};
+	}
+
 	render(){
 		return (
-			<h3>Request List</h3>
+			<Layout>
+				<h3>Request List</h3>
+
+				<Link route={`/campaigns/${this.props.address}/requests/new`}>
+					<a>
+						<Button primary>Add Request</Button>
+					</a>
+				</Link>
+
+			</Layout>
 		);
 	}
 }
