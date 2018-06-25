5cd96c6422d2b54fc07eeeb3f2e6205e3e3939ea
sdlfkorg <sdlfkorg@gmail.com>
2018-06-25 20:45:22 +0800

    components-RequestRow: styling the button and row for different cases

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -12,18 +12,34 @@ class RequestRow extends Component {
 		});
 	}
 
+	onFinalize = async () => {
+		const campaign = Campaign(this.props.address);
+		const accounts = await web3.eth.getAccounts();
+		await campaign.methods.finalizeRequest(this.props.id).send({
+			from: accounts[0]
+		});	
+	}
+
 	render(){
 		const {Row, Cell} = Table;
 		const {id, request, approversCount} = this.props;
+		const readyToFinalized = request.approvalCount > approversCount / 2;
 		return (
-			<Row>
+			<Row disabled={request.complete} positive={readyToFinalized && !request.complete} >
 				<Cell>{id}</Cell>
 				<Cell>{request.description}</Cell>
 				<Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
 				<Cell>{request.recipient}</Cell>
 				<Cell>{request.approvalCount}/{approversCount}</Cell>
 				<Cell>
-					<Button color="green" basic onClick={this.onApprove}>Approve</Button>
+					{request.complete ? null : (
+						<Button color="green" basic onClick={this.onApprove}>Approve</Button>
+					)}
+				</Cell>
+				<Cell>
+					{request.complete ? null :(
+						<Button color="teal" basic onClick={this.onFinalize}>Finalize</Button>
+					)}
 				</Cell>
 			</Row>
 		);
