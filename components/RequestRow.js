64146d2e14ea9be9c84b71d667d5027c7c56f904
sdlfkorg <sdlfkorg@gmail.com>
2018-06-25 18:00:08 +0800

    components: add approve button for approve the request

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react';
-import {Table} from 'semantic-ui-react';
+import {Button, Table} from 'semantic-ui-react';
 import web3 from '../ethereum/web3';
+import Campaign from '../ethereum/campaign';
 
 class RequestRow extends Component {
+	onApprove = async () => {
+		const campaign = Campaign(this.props.address);
+		const accounts = await web3.eth.getAccounts();
+		await campaign.methods.approveRequest(this.props.id).send({
+			from: accounts[0]
+		});
+	}
+
 	render(){
 		const {Row, Cell} = Table;
 		const {id, request, approversCount} = this.props;
@@ -13,7 +22,9 @@ class RequestRow extends Component {
 				<Cell>{web3.utils.fromWei(request.value, 'ether')}</Cell>
 				<Cell>{request.recipient}</Cell>
 				<Cell>{request.approvalCount}/{approversCount}</Cell>
-				{/*<Cell>{request.}</Cell>*/}
+				<Cell>
+					<Button color="green" basic onClick={this.onApprove}>Approve</Button>
+				</Cell>
 			</Row>
 		);
 	}
