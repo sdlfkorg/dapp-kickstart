a41afd6a2acf29e49f25346180726b4dbfe44542
sdlfkorg <sdlfkorg@gmail.com>
2018-06-25 16:20:28 +0800

    page-request-index: get each request info

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,11 +2,23 @@ import React, {Component} from 'react';
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
 import {Button} from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
 	static async getInitialProps(props){
 		const {address} = props.query;
-		return {address: address};
+		const campaign = Campaign(address);
+
+		const requestCount = await campaign.methods.getRequestsCount().call();
+
+		const requests = await Promise.all(
+			Array(parseInt(requestCount)).fill().map((element, index) => {
+				return campaign.methods.requests(index).call();
+			})
+		);
+		console.log(requests)
+
+		return {address, requests, requestCount};
 	}
 
 	render(){
