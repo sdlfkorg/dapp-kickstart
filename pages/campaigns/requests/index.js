7ea5f7d4311062314dbf1221b814442ce17e844a
sdlfkorg <sdlfkorg@gmail.com>
2018-06-25 16:54:00 +0800

    pages: show a basic requests table

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import Layout from '../../../components/Layout';
 import { Link } from '../../../routes';
-import {Button} from 'semantic-ui-react';
+import {Button, Table} from 'semantic-ui-react';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
 	static async getInitialProps(props){
@@ -21,7 +22,21 @@ class RequestIndex extends Component {
 		return {address, requests, requestCount};
 	}
 
+	renderRows(){
+		return this.props.requests.map((request, index) => {
+			return (
+				<RequestRow 
+					key={index}
+					request={request} 
+					id={index}
+					address={this.props.address} 
+				/>
+			)
+		});
+	}
+
 	render(){
+		const {Header, Row, HeaderCell, Body} = Table;
 		return (
 			<Layout>
 				<h3>Request List</h3>
@@ -31,6 +46,24 @@ class RequestIndex extends Component {
 						<Button primary>Add Request</Button>
 					</a>
 				</Link>
+
+				<Table>
+					<Header>
+						<Row>
+							<HeaderCell>ID</HeaderCell>
+							<HeaderCell>Description</HeaderCell>
+							<HeaderCell>Amount</HeaderCell>
+							<HeaderCell>Recipient</HeaderCell>
+							<HeaderCell>Approval</HeaderCell>
+							<HeaderCell>Approve</HeaderCell>
+							<HeaderCell>Finalize</HeaderCell>
+						</Row>
+					</Header>
+					<Body>
+						{this.renderRows()}
+					</Body>
+				</Table>
+
 
 			</Layout>
 		);
